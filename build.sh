f1a3ea9c00139ce660365ca01edeb7ff83179e5d
Aditya Poojari <adityapoojari261@gmail.com>
2026-08-15 16:40:04 +0530

    Fix Render build command

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-set -o errexit
 
 pip install -r requirements.txt
-python django_web_app/manage.py collectstatic --no-input
-python django_web_app/manage.py migrate
+
+cd django_web_app
+
+python manage.py collectstatic --no-input
+python manage.py migrate
